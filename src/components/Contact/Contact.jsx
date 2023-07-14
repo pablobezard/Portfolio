b081e5a8498be4244aeb3cd848c81fa319f54484
pablobezard <pablobezard@gmail.com>
2023-07-14 01:42:23 -0300

    project

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './contact.css';
+import { motion } from 'framer-motion';
+import AnimatedCharacter from '../AnimatedCharacter/AnimatedCharacter';
+
+const Contact = () => {
+  return (
+    <motion.div
+      className="contact"
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 100 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h2>Contact</h2>
+      <div className="contact-info">
+        <p>Email: Pablobezard@gmail.com</p>
+        <p>Teléfono: +54 3534789688</p>
+      </div>
+      <div className="contact-content">
+        <form className="contact-form">
+          <input type="text" placeholder="Nombre" />
+          <input type="email" placeholder="Correo electrónico" />
+          <textarea placeholder="Mensaje"></textarea>
+          <button type="submit">Enviar</button>
+        </form>
+        <div className="contact-animation">
+          <AnimatedCharacter />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Contact;
