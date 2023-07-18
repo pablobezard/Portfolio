0293ac479414a63005eeccd94b279e90a7ca3019
pablobezard <pablobezard@gmail.com>
2023-07-18 13:05:10 -0300

    json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Projects from './components/Projects/Projects';
 import Skills from './components/Skills/Skills';
