7bb4042e549fa3f6a9cc888583f370f95a219e8e
pablobezard <pablobezard@gmail.com>
2024-02-23 18:36:20 -0300

    i18n

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {I18nextProvider} from 'react-i18next'
+import i18next from 'i18next';
+
+import global_en from './translations/en/global.json';
+import global_es from './translations/es/global.json';
+import global_do from './translations/do/global.json';
+
+i18next.init({
+  interpolation: {escapeValue: false},
+});  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
